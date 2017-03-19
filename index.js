4ff8665fd81a14dd9e794ccea90a3d823cc1c20c
gee2789 <geeyung.li@gmail.com>
2017-03-19 16:16:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+
+// const sortedRobots = robots.map(function(robot){
+//     if (knownDecepticons.includes(robot.name){
+//         return Object.assign({}, robot, (
+//           alliance: 'decepticon')
+//     }else{
+//         return Object.assign({}, robot, (
+//           alliance: 'autobot')
+//     )});
+//   ));
+// })
+
+
 const knownDecepticons = ['Megatron', 'Skywarp', 'Laserbeak', 'Barricade'];
 
 const robots = [
@@ -11,6 +25,13 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(robot => {
+  const isDecepticon = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+    alliance: isDecepticon ? 'decepticon' : 'autobot'
+  });
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +42,10 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map((stripe,index) => {
+  const isEven = (index % 2) === 0
+  return Object.assign({}, stripe, {
+    color: isEven? 'black':'white'
+  })
+})
